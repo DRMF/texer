6bec8f6376935a794559097a3d8af954e1ebd78e
Jagan Prem <notjagan@gmail.com>
2016-04-19 16:28:09 -0400

    Fixed the test cases for mathmode.js.

--- test/mathmodetest.js
+++ test/mathmodetest.js
@@ -1,16 +1,16 @@
 "use strict";
 var assert = require('assert');
-var mathFinder = require('../mathmode.js');
+var mathFinder = require('../lib/mathmode.js');
 var testcases = [
-    {input: '', options: '', out: ''},
+    {input: '', options: '', out: []},
     {
         input: 'This is a test',
         options: {},
-        out: 'This is a test'
-    }
+        out: []
+    },
     {
-        input : '$a$ and $b$';
-        options: {};
+        input : '$a$ and $b$',
+        options: {},
         out: ['a', 'b']
     }
 ];
@@ -21,7 +21,7 @@ describe('Index', function () {
         var options = tc.options;
         var output = tc.out;
         it('should correctly replace ' + JSON.stringify(input), function () {
-            assert.deepEqual(mathmode.findRanges(input), output);
+            assert.deepEqual(mathFinder.findRanges(input), output);
         });
     });
 });
