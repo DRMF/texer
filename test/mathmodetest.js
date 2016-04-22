0f276f45efa08e0506077a481741aee0262591b9
notjagan <notjagan@gmail.com>
2016-04-22 18:05:10 -0400

    Added a NodeJS program to find occurrences of math mode. (#2)
    * Added a NodeJS program to find occurences of math mode.
    
    * Added a NodeJS program to find occurrences of math mode.
    
    * Corrected the majority of the lint errors in mathmode.js (originally texvcjs_readinfile.js). Also added a test case for mathmode.js under test/mathmodetest.js.
    
    * Fixed the test cases for mathmode.js.
    
    * Added more test cases to mathmodetest.js.
    
    * Removed unnecessary code from mathmode.js and added more coverage to tests.
    
    * Cleaned up some indentation inconsistencies, and removed more redundant code.
    
    * Removed .idea files (I think) and added to .gitignore.

---
+++ test/mathmodetest.js
@@ -0,0 +1,42 @@
+"use strict";
+var assert = require('assert');
+var mathFinder = require('../lib/mathmode.js');
+var testcases = [
+    {input: '', options: '', out: []},
+    {
+        input: 'This is a test',
+        options: {},
+        out: []
+    },
+    {
+        input: '$a$ and $b$',
+        options: {},
+        out: ['a', 'b']
+    },
+    {
+        input: '$c\\hbox{and $d$}$',
+        options: {},
+        out: ['c', 'd']
+    },
+    {
+        input: '\\$Not this\\$ \\\\[or this\\\\] \\\\(or even this\\\\) $This$',
+        options: {},
+        out: ['This']
+    },
+    {
+        input: '$math mode\\hbox{not math {still not math}}$',
+        options: {},
+        out: ['math mode']
+    }
+];
+
+describe('Index', function () {
+    testcases.forEach(function (tc) {
+        var input = tc.input;
+        var options = tc.options;
+        var output = tc.out;
+        it('should correctly replace ' + JSON.stringify(input), function () {
+            assert.deepEqual(mathFinder.findMathSections(input), output);
+        });
+    });
+});
